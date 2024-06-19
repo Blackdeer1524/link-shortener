84193d4d9d9a6c894b9ca0357fc98c7d4f9a2ea8
Danila Slesarev <blackdeer1524@yandex.ru>
2024-06-19 16:49:55 +0300

    init db script for docker

---
+++ server/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Users (
+    Id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    Name VarChar(300) NOT NULL,
+    Email VarChar(80)  NOT NULL,
+    HashedPassword CHAR(60) NOT NULL
+)
+;
+
+CREATE TABLE Urls (
+	ShortUrl VarChar(5) PRIMARY KEY,
+	LongUrl VarChar(300) NOT NULL,
+	UserId uuid NOT NULL references Users(Id),
+	ExpirationDate Timestamp NOT NULL DEFAULT now() + interval '30' day
+)
+;
